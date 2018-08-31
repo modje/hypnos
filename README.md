59f708f0fba7d428bf6235051ff4bfd9504e0a47
modje <mojinius@gmail.com>
2018-08-31 11:40:42 +0200

    README add installation process, POC with selenium and PhantomJS

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # hypnos
 Youtube music download automation with youtube-dl
+
+## Install pre-requisites
+* PIP
+curl "https://bootstrap.pypa.io/get-pip.py" -o get-pip.py
+python get-pip.py
+python -m pip install --upgrade pip setuptools wheel
+* Selenium
+python -m pip install selenium
+* PhantomJS
+http://phantomjs.org/download.html
+(extract binary in folder contained in PATH)
--- hypnos.py
+++ hypnos.py
@@ -1,27 +1,68 @@
-import urllib, json
-author = 'Youtube_Username'
+#import urllib.request
+#import json
+#from lxml import html
+#from urllib.request import urlopen
 
-foundAll = False
-ind = 1
-videos = []
-while not foundAll:
-    inp = urllib.urlopen(r'http://gdata.youtube.com/feeds/api/videos?start-index={0}&max-results=50&alt=json&orderby=published&author={1}'.format( ind, author ) )
-    try:
-        resp = json.load(inp)
-        inp.close()
-        returnedVideos = resp['feed']['entry']
-        for video in returnedVideos:
-            videos.append( video ) 
+from selenium import webdriver
+#from selenium.webdriver.common.by import By
 
-        ind += 50
-        print len( videos )
-        if ( len( returnedVideos ) < 50 ):
-            foundAll = True
-    except:
+#from lxml.etree import tostring
+
+
+driver = webdriver.PhantomJS()
+driver.get('https://www.youtube.com/user/sprocker7/videos')
+#p_element = driver.find_element_by_id(id_='video-title')
+#print(p_element.text)
+links = driver.find_elements_by_xpath('//body//a')
+for link in links:
+	print(link.text)
+
+#doc = html.parse(urlopen('https://www.youtube.com/user/sprocker7/videos'))
+
+
+#persons = []
+#for person in driver.find_elements_by_class_name('person'):
+#    title = person.find_element_by_xpath('.//div[@class="title"]/a').text
+#    company = person.find_element_by_xpath('.//div[@class="company"]/a').text
+
+#    persons.append({'title': title, 'company': company})
+
+
+#print(tostring(doc))
+
+
+#doc = html.parse('https://www.youtube.com/user/sprocker7/videos')
+#doc = html.parse('http://modjecart.free.fr/index.php')
+
+#res = doc.xpath("//a[@id='video-title']")
+#print(res)
+
+
+#author = 'sprocker7'
+
+#foundAll = False
+#ind = 1
+#videos = []
+#while not foundAll:
+
+#with urllib.request.urlopen('http://gdata.youtube.com/feeds/api/videos?start-index={0}&max-results=50&alt=json&orderby=published&author={1}'.format(ind,author)) as url:
+#    inp = url.read()
+#    try:
+#resp = json.load(inp)
+#inp.close()
+#        returnedVideos = resp['feed']['entry']
+#        for video in returnedVideos:
+#            videos.append( video ) 
+
+        #ind += 50
+        #print len(videos)
+        #if ( len( returnedVideos ) < 50 ):
+        #    foundAll = True
+#    except:
         #catch the case where the number of videos in the channel is a multiple of 50
-        print "error"
-        foundAll = True
+#        print "error"
+#        foundAll = True
 
-for video in videos:
-    print video['title'] # video title
-    print video['link'][0]['href'] #url
+#for video in videos:
+#    print video['title'] # video title
+#    print video['link'][0]['href'] #url
